9f43a9f360ae43af73a2d9587f3dcf430a3b476b
Daniel Olsson <glajan@gmail.com>
2020-09-16 13:21:48 +0200

    adf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Todos from "./Todos";
 import Header from "./Header";
 import AddTodo from "./AddTodo";
